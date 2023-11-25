9d2c305dbec147db168087fd635137cc9ab93ea4
RMO97 <belmont2097@gmail.com>
2023-11-25 16:21:15 -0600

    Esqueleto del API

---
+++ APICuponera/src/java/ws/AutenticacionWS.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Richard
+ */
+@Path("autenticacion")
+public class AutenticacionWS {
+
+    @Context
+    private UriInfo context;
+
+    
+    public AutenticacionWS() {
+    }
+
+
+}
